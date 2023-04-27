0e99ba4bd70325e6537711234531c03fc8ab65a7
Lingo34 <lingo34@skiff.com>
2023-04-27 11:19:10 -0700

    stable | 无头 | 适配东流小说bifengzw.com ｜ 大量debug
    - 添加了有限的反反爬虫机制, 并尝试改用无头模式
    
    - 优化档案写入writeFile 报错与重试流程, 改用递归处理写入错误重试
    
    - 尝试修正 bug: "000介绍文件第一次执行时无法创建, 显示父目录不存在: ENOENT: no such file or directory, open", 因为原本的资料夹创建 fs.mkdir 不是 sync, 所以创建 000 介绍文件时资料夹还没有创建, 解决方法是使用 fs.mkdirSync 而非 fs.mkdir
    
    - 不再列出书源目录下所有文件
    
    - 修改了小说正文的处理方式. 现在小说正文的格式化由书源负责, 但小说简介因为获取时并未获取书源, 使用了原本的格式化函数
    
    - xbiquge.tw 的正文格式化 的删除水印改进, 现在使用正则表达式, 不需要bookname 来移除水印
    
    - 现在正文格式化有 trim 的功能了
    
    - 添加了书源version, 目前是 v1.0.0
    
    - 匹配书源改用 .bookSourceName 属性, 而不是搜索整个书源文件

--- index.js
+++ index.js
@@ -32,16 +32,16 @@ async function main() {
     // 书源文件
     let bookSourceName;
     // 列出所有书源文件
-    console.log("\n>> 列出所有书源文件 << \n")
-    fs.readdirSync('./bookSource', (err, files) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        files.forEach(file => {
-            console.log(file);
-        });
-    });
+    // console.log("\n>> 列出所有书源文件 << \n")
+    // fs.readdirSync('./bookSource', (err, files) => {
+    //     if (err) {
+    //         console.error(err);
+    //         return;
+    //     }
+    //     files.forEach(file => {
+    //         console.log(file);
+    //     });
+    // });
 
     bookSourceName = prompt('請指定书源文件, 不指定则根据url自动匹配: ')
     // bookSourceName = './bookSource/' + bookSourceName
@@ -62,8 +62,15 @@ async function getBook(url, startIndex, endIndex, dir, bookSourceName) {
     // 启动浏览器
     const browser = await puppeteer.launch(
         {
-            headless: false, // 默认是无头模式，这里为了示范所以使用正常模式
+            headless: "new", // 默认是无头模式，new 是新版无头
             //args: ["--user-data-dir=./chromeTemp"]
+            args: [
+                // "--user-data-dir=./chromeTemp" // 保存登录状态
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-blink-features=AutomationControlled',
+            ],
+            dumpio: false,
         }
     )
 
@@ -91,18 +98,26 @@ async function getBook(url, startIndex, endIndex, dir, bookSourceName) {
     console.log("获取书源...")
 
     bookSource = Hjson.parse(fs.readFileSync(bookSourcePath, 'utf8'))
-    console.log("\n书源: " + JSON.stringify(bookSource, null, 4) + "\n\n")
+
+    // console.log("\n书源已确定: " + JSON.stringify(bookSource, null, 4) + "\n\n")
+    console.log(`\n书源已确定: "${bookSource.bookSourceName}", "${bookSource.bookSourceUrl}"\n\n`)
 
     // 将 json 中的两个 函数字符串转换为可执行函数
     // 但这两个函数字符串转换必须要放在 下面的 page.evaluate 回调函数中，否则会报错
     // bookSource.getBookInfo = new Function(bookSource.getBookInfo.arguments, bookSource.getBookInfo.body);
     // bookSource.getContent = new Function(bookSource.getContent.arguments, bookSource.getContent.body);
-    // 不过转换个 test 无伤大雅
-    bookSource.test = new Function(bookSource.test.arguments, bookSource.test.body);
+
+    // 这里初始化一下不在 page.evaluate 中使用的工具函数
     // 从章节url中获取书籍id的函数
     bookSource.getBookID = new Function(bookSource.getBookID.arguments, bookSource.getBookID.body);
     // 获取书籍主页url的函数
     bookSource.getHomeUrl = new Function(bookSource.getHomeUrl.arguments, bookSource.getHomeUrl.body);
+    // 格式化小说正文内容的函数
+    bookSource.formatContentText = new Function(bookSource.formatContentText.arguments, bookSource.formatContentText.body);
+    
+    // 书源测试函数
+    bookSource.test = new Function(bookSource.test.arguments, bookSource.test.body);
+
 
     console.log("浏览器已啟動, 获取书籍信息...")
 
@@ -132,38 +147,45 @@ async function getBook(url, startIndex, endIndex, dir, bookSourceName) {
     {
         n = prompt('按任意鍵繼續... 输入 n 退出')
         if (n == 'n') {
+            console.log("退出程序...")
             browser.close()
             return;
         }
     }
 
     // 創建資料夾
-    fs.mkdir(`${dir}`, function (err) {
-        if (err) {
+    // fs.mkdirSync(`${dir}`, function (err) {
+    // })
+
+    try {
+        fs.mkdirSync(`${dir}`) // 创建小说名稱資料夾
+        console.log(`创建目录"${dir}"成功。`)
+    } catch (error) {
+        if(error.code == 'EEXIST')
             console.log("目录已存在，无需创建。")
-            console.log(err)
-        } else {
-            console.log(`创建目录"${dir}"成功。`)
+        else
+        {
+            console.log("创建目录失败，未知错误... 打印错误信息:")
+            console.log(error)
         }
-    })
+    }
+
+
 
     // 如果从小说第一页开始爬取，添加 000 小说介绍文件, <= 1 是因为可以填负数
     if (startIndex <= 1) {
         console.log("创建 000 介绍文件...")
-        try {
-
-            // 寫入 000 介绍檔案
-            writeFile(`${dir}`, `000.txt`, JSON.stringify(bookInfo, null, 4),
-                ` --> ${bookInfo.bookname} / 000 介绍文件 已儲存`, ` #####! <-- 000 介绍文件寫入錯誤 !!!!!  退出程序...######`)
-        } catch (error) {
-
-        }
+        
+        // 寫入 000 介绍檔案
+        writeFile(`${dir}`, `000.txt`, JSON.stringify(bookInfo, null, 4),
+            ` --> ${bookInfo.bookname} / 000 介绍文件 已儲存`, ` #####! <-- 000 介绍文件寫入錯誤 !!!!!  退出程序...######`)
     }
 
     // 控制浏览器打开新标签页面
     const page = await browser.newPage()
     console.log("标签页已啟動, 开始爬取小说内容...")
 
+    let lastPageData = null; // 上一章的页面
     // 循环爬取小说每一章
     for (pageNum = startIndex; pageNum <= endIndex; pageNum++) {
         // 前往章节页面
@@ -180,7 +202,7 @@ async function getBook(url, startIndex, endIndex, dir, bookSourceName) {
             return;
         })
 
-        let data = null; // store the data of the page, js object, {content, title, nextPageUrl}
+        let contentPageData = null; // store the data of the page, js object, {content, title, nextPageUrl}
         // 在新标签中打开要爬取的网页
 
         // 如果出错，要重試10次
@@ -194,10 +216,12 @@ async function getBook(url, startIndex, endIndex, dir, bookSourceName) {
             try {
                 // 使用evaluate方法在浏览器中执行传入函数（完全的浏览器环境，所以函数内可以直接使用window、document等所有对象和方法）
                 // =================== 此处已被替换为hjson, 为content evaluate 函数 ===================
-                data = await page.evaluate((bookSource) => {
+                contentPageData = await page.evaluate((bookSource) => {
                     bookSource.getContent = new Function(bookSource.getContent.arguments, bookSource.getContent.body);
                     return bookSource.getContent(document);
                 }, bookSource)
+                // 格式化小说正文内容
+                contentPageData.content = bookSource.formatContentText(contentPageData.content)
             } catch (err) {
                 if (page.url() == bookInfo.homeUrl) {
                     console.log(" #####! <-- 爬取完畢, 本页是目录页: home url.  退出程序...######")
@@ -211,19 +235,28 @@ async function getBook(url, startIndex, endIndex, dir, bookSourceName) {
             break;
         }
 
+        // 如果本页标题与上一页标题相同, 应当是同章分页, 合并内容
+        if(lastPageData && contentPageData.title == lastPageData.title){
+            console.log(` <-- ! ${dir} 第${pageNum}章: ${contentPageData.title} - 與上一章 ${pageNum-1} 標題相同, 写入同一个文件`)
+            contentPageData.content = lastPageData.content + '\n' + contentPageData.content // 合并内容
+            pageNum--; // 页数减一
+            
+        }
+
         // 寫入檔案
         //          (目录位置/, 檔案名稱, 檔案內容, 成功訊息, 失敗訊息)
-        writeFile(`${dir}`, `${pageNum.toString().padStart(2, '0')} ${data.title}.txt`,
-            convertToPlainText(data.content, bookname),
-            ` --> ${dir} 第${pageNum}章: ${data.title} 已儲存`,
+        writeFile(`${dir}`, `${pageNum.toString().padStart(2, '0')} ${contentPageData.title}.txt`,
+            contentPageData.content,
+            ` --> ${dir} 第${pageNum}章: ${contentPageData.title} 已儲存`,
             ` #####! <-- ${dir}/ 第${pageNum}章: 寫入錯誤或data為空 !!!!!  退出程序...######`)
 
         // 前往下一页
-        url = data.nextPageUrl;
+        url = contentPageData.nextPageUrl;
+        lastPageData = contentPageData;
 
         if (url == null || url == bookInfo.homeUrl) {
             console.log(` #####! <-- 爬取完畢, 本页是目录页: url == "${url}."  退出程序...######`)
-            return;
+            break;
         }
 
         //await sleep(200);
@@ -242,7 +275,7 @@ async function getBook(url, startIndex, endIndex, dir, bookSourceName) {
 
 //! ---- Novel Tools ----
 // 根据小说目录页的url，推断小说源, 并返回小说源文件的相对路径, 如果找不到，返回null
-async function deduceBookSourceFromUrl(url, bookSourceDir) //!! 未完成 ================================== OOOOO
+async function deduceBookSourceFromUrl(url, bookSourceDir)
 {
     url = new URL(url);
 
@@ -250,7 +283,7 @@ async function deduceBookSourceFromUrl(url, bookSourceDir) //!! 未完成 ======
     // ? 我想删除子域名, 只搜索主域名, 但是不知道怎么做
     let host = url.hostname.replace("www.", "");
 
-    let bookSource = findStringInFiles(bookSourceDir, host)[0]
+    let bookSource = findBookSourceByHost(bookSourceDir, host)[0]
     if (!bookSource || bookSource == "") {
         console.error(" #####! <-- 未找到匹配的小说源, 退出程序...######")
         return null;
@@ -264,8 +297,13 @@ async function deduceBookSourceFromUrl(url, bookSourceDir) //!! 未完成 ======
 // 返回 {bookname, img, author, intro, homeUrl}
 async function getBookInfo(browser, bookSource, chapterUrl) {
 
+    console.log(" --- 正在获取小说信息 --- ")
     // 获取小说主页url
     let homeUrl = bookSource.getHomeUrl(bookSource.getBookID(chapterUrl));
+    console.log(" Home Url: " + homeUrl)
+
+    // Logx
+    //console.log(await getFullHtml(browser, homeUrl));
 
     // 控制浏览器打开新标签页面
     const page = await browser.newPage()
@@ -293,7 +331,7 @@ async function getBookInfo(browser, bookSource, chapterUrl) {
         throw err;
     }
     data.homeUrl = homeUrl;
-    data.intro = convertToPlainText(data.intro, data.bookname);
+    data.intro = convertToPlainText(data.intro);
 
     page.close(); // 关闭页面
     return data;
@@ -302,18 +340,58 @@ async function getBookInfo(browser, bookSource, chapterUrl) {
 
 
 //convert html to plain text
-function convertToPlainText(str, bookname) {
+function convertToPlainText(str) {
     try {
         // 將 <br/> 和 <br> 轉換為換行符
         str = str.replace(/<br\s*\/?>/g, "\n");
         // 將 &nbsp; 轉換為空格
         str = str.replace(/&nbsp;/g, "");
-        // 刪除首行水印
-        str = str.replace(new RegExp(`笔趣阁 www.xbiquge.tw，最快更新${bookname} ！`), '');
     } catch (err) {
         console.log(err)
     }
     return str;
+}
+
+async function getFullHtml(browser, url)
+{
+
+    // 控制浏览器打开新标签页面
+    const page = await browser.newPage()
+    await page.goto(url)
+
+    let data = null;
+
+    // await page.screenshot({
+    //     path: 'jojojojo.png',
+    //     type: 'png',
+    //     // quality: 100, 只对jpg有效
+    //     fullPage: true,
+    //     // 指定区域截图，clip和fullPage两者只能设置一个
+    //     // clip: {
+    //     //   x: 0,
+    //     //   y: 0,
+    //     //   width: 1000,
+    //     //   height: 40
+    //     // }
+    //   });
+
+    
+
+    try {
+        // 使用evaluate方法在浏览器中执行传入函数
+        //  ==================== 已被替换为 hjson get book info. 为 evaluate 函数 ====================
+        data = await page.evaluate(() => {
+
+            return document.body.innerHTML; // 直接回传页面的html, 用于测试
+        })
+    } catch (err) {
+        console.error(`\n\n #####! <-- 页面 "${url}" 爬取失敗，正在报错...\n\n`)
+        throw err;
+    }
+    // console,log(data)
+    page.close(); // 关闭页面
+
+    return data;
 }
 
 
@@ -324,9 +402,9 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// 递归查找目录下的所有文件中是否包含指定字符串
-// 返回包含指定字符串的文件路径数组
-function findStringInFiles(dir, str) {
+// 递归查找目录下的所有hjson 文件中的bookSourceUrl 字段是否包含host
+// 返回包含指定host字符串的书源文件路径数组
+function findBookSourceByHost(dir, host) {
     const files = fs.readdirSync(dir);
     const result = [];
 
@@ -335,11 +413,11 @@ function findStringInFiles(dir, str) {
         const stat = fs.statSync(filePath);
 
         if (stat.isDirectory()) {
-            result.push(...findStringInFiles(filePath, str));
+            result.push(...findBookSourceByHost(filePath, host));
         } else if (stat.isFile() && path.extname(file) === '.hjson') {
-            const content = fs.readFileSync(filePath, 'utf8');
+            const currentBookSource = Hjson.parse( fs.readFileSync(filePath, 'utf8') );
 
-            if (content.indexOf(str) !== -1) {
+            if (currentBookSource.bookSourceUrl.indexOf(host) !== -1) {
                 return result.push(filePath);
             }
         }
@@ -348,35 +426,46 @@ function findStringInFiles(dir, str) {
     return result;
 }
 
-// 封装fs.writeFile
-// dir: 檔案目录, fileName: 檔案名稱, data: 檔案內容, successMessage: 成功訊息, errMessage: 失敗訊息
-function writeFile(dir, fileName, data, successMessage, errMessage) {
+// 封装fs.writeFile, 如果写入错误, 递归重试10次
+// dir: 檔案目录, fileName: 檔案名稱, data: 檔案內容, 
+// successMessage: 成功訊息, errMessage: 失敗訊息, 
+// attempts: 重試次數, 调用时不用传参
+function writeFile(dir, fileName, data, successMessage, errMessage, attempts = 1) {
+    
     // 如果失败, 重試10次
-    for (let i = 1; i <= 11; i++) {
-        try {
-            fs.writeFile(`${dir}${sanitizeFileName(fileName)}`, data, function (err) {
-                if (err) {
-                    console.log(errMessage)
-                    throw err
-                };
-                console.log(successMessage);
-                return;
-            });
-            return; // 成功寫入後退出
-
-        } catch (err) {
-            if (i >= 10) {
+    fs.writeFile(`${dir}${sanitizeFileName(fileName)}`, data, function (err) {
+        if (err) {
+            console.log(errMessage)
+            console.log(err);
+            if (attempts >= 10) {
                 console.log(` #####! --- 寫入錯誤, 第10重试失敗, 退出程序... ######`)
-                console.error(err);
-                return
+                return false
             }
-            console.log(` #####! --- 寫入錯誤, 正在重试: ${i}/10 ######`)
-            sleep(500); // 等待0.5秒后重试
-            continue;
-        }
+            else
+            {
+                console.log(` #####! --- 寫入錯誤, 正在重试: ${attempts+1}/10 ######`)
+                return writeFile(dir, fileName, data, successMessage, errMessage, attempts + 1);
+            }
+        };
+        console.log(successMessage);
+        return true;
+    });
 
-    }
-    console.log(` #####! --- 未知问题发生，此处不应被抵达, 看起来似乎有bug... ######`)
+        
+        
+        // try {
+            
+
+        // } catch (err) {
+        //     if (i >= 10) {
+        //         console.log(` #####! --- 寫入錯誤, 第10重试失敗, 退出程序... ######`)
+        //         console.error(err);
+        //         return
+        //     }
+        //     console.log(` #####! --- 寫入錯誤, 正在重试: ${i}/10 ######`)
+        //     sleep(500); // 等待0.5秒后重试
+        //     continue;
+        // }
 }
 
 
